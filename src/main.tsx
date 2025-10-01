42f5f15e6372f4096e928a25d22c04da5de7aeb9
roguegrid9 <team@roguegrid9.com>
2025-09-30 22:15:55 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import AppShell from './shell/AppShell'
 import Home from './routes/Home'
-import HowItWorks from './routes/HowItWorks'
-import Download from './routes/Download'
-import Pricing from './routes/Pricing'
-import Docs from './routes/Docs'
-import Roadmap from './routes/Roadmap'
-import PressKit from './routes/PressKit'
 import NotFound from './routes/NotFound'
 
 const router = createBrowserRouter([
@@ -19,12 +13,6 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'how-it-works', element: <HowItWorks /> },
-      { path: 'download', element: <Download /> },
-      { path: 'pricing', element: <Pricing /> },
-      { path: 'docs', element: <Docs /> },
-      { path: 'roadmap', element: <Roadmap /> },
-      { path: 'press-kit', element: <PressKit /> },
     ]
   },
 ])
